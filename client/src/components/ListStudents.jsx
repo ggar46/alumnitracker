a772abd218c81141ad491c555a3188010ddf339b
Gisselle Garcia <gissellegar46@gmail.com>
2023-06-05 16:54:34 -0700

    Change card import

--- client/src/components/ListStudents.jsx
+++ client/src/components/ListStudents.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import * as ioicons from "react-icons/io5";
 import MyForm from "./Form";
-import Student from "./Student";
+import Alumnus from "./Alumnus";
 
 const ListStudents = () => {
   // this is my original state with an array of students
