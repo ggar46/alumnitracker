d5c8f193515e3696162c550b82d3fbb7de51ae69
Dana <dabinkim807@gmail.com>
2023-06-05 17:44:26 -0400

    add is_converted attribute to routes

--- server/server.js
+++ server/server.js
@@ -25,8 +25,8 @@ app.get('/api/alumni', async (req, res) => {
 app.post('/api/alumni', async (req, res) => {
   try {
     const result = await db.query(
-      "INSERT INTO alumni(name, position, company, salary, start_date, is_looking, linkedin) VALUES($1, $2, $3, $4, $5, $6, $7)",
-      [req.body.name, req.body.position, req.body.company, req.body.salary, req.body.start_date, req.body.is_looking, req.body.linkedin],
+      "INSERT INTO alumni(name, position, company, salary, start_date, is_looking, linkedin, is_converted) VALUES($1, $2, $3, $4, $5, $6, $7, $8)",
+      [req.body.name, req.body.position, req.body.company, req.body.salary, req.body.start_date, req.body.is_looking, req.body.linkedin, req.body.is_converted],
     );
     const returnObj = {
       id: result.rows[0].id,
@@ -36,7 +36,8 @@ app.post('/api/alumni', async (req, res) => {
       salary: req.body.salary,
       start_date: req.body.start_date,
       is_looking: req.body.is_looking,
-      linkedin: req.body.linkedin
+      linkedin: req.body.linkedin,
+      is_converted: req.body.is_converted
     }
     return res.status(200).json(returnObj);
   } catch (e) {
@@ -48,8 +49,8 @@ app.put('/api/alumni/:alumnusId', async (req, res) =>{
   const id = parseInt(req.params.alumnusId);
   try {
     await db.query(
-			"UPDATE alumni SET name = $1, position = $2, company = $3, salary = $4, start_date = $5, is_looking = $6, linkedin = $7 WHERE id = $8", 
-			[req.body.name, req.body.position, req.body.company, req.body.salary, req.body.start_date, req.body.is_looking, req.body.linkedin, id]
+			"UPDATE alumni SET name = $1, position = $2, company = $3, salary = $4, start_date = $5, is_looking = $6, linkedin = $7, is_converted = $8 WHERE id = $9", 
+			[req.body.name, req.body.position, req.body.company, req.body.salary, req.body.start_date, req.body.is_looking, req.body.linkedin, req.body.is_converted, id]
 		);
 	} catch(e) {
 		return res.status(400).send(String(e));
