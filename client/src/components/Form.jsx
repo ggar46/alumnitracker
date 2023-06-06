1421ea38790696e24fe99a6d4513d32b6af71e71
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-06-05 18:13:12 -0600

    fixing form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,122 +1,208 @@
-import React, { useState, useEffect } from 'react'
-import { Button, Form } from "react-bootstrap"
+import React, { useState, useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
 
-const MyForm = ({ onSaveStudent, editingStudent, onUpdateStudent }) => {
-
-    // This is the original State with not initial student 
-    const [student, setStudent] = useState(editingStudent || {
-        firstname: "",
-        lastname: "",
-        is_current: false
-    });
-
-    //create functions that handle the event of the user typing into the form
-    const handleNameChange = (event) => {
-        const firstname = event.target.value;
-        setStudent((student) => ({ ...student, firstname }));
-
-    };
-
-    const handleLastnameChange = (event) => {
-        const lastname = event.target.value;
-        setStudent((student) => ({ ...student, lastname }));
-    };
-
-    const handleCheckChange = (event) => {
-        const is_current = event.target.checked;
-        //console.log(iscurrent);
-        setStudent((student) => ({ ...student, is_current }));
-    };
-
-    const clearForm = () => {
-        setStudent({ firstname: "", lastname: "", is_current: false })
+const MyForm = ({ onSaveAlumnus, editingAlumnus, onUpdateAlumnus }) => {
+  // This is the original State with not initial student
+  const [alumnus, setAlumnus] = useState(
+    editingAlumnus || {
+      name: "",
+      position: "",
+      company: "",
+      salary: "",
+      date: "",
+      is_looking: false,
     }
+  );
 
-    //A function to handle the post request
-    const postStudent = (newStudent) => {
-        return fetch("http://localhost:8080/api/students", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(newStudent),
-        })
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                //console.log("From the post ", data);
-                //I'm sending data to the List of Students (the parent) for updating the list
-                onSaveStudent(data);
-                //this line just for cleaning the form
-                clearForm();
-            });
-    };
+  //create functions that handle the event of the user typing into the form
+  const handleNameChange = (event) => {
+    const name = event.target.value;
+    setAlumnus((alumnus) => ({ ...alumnus, name }));
+  };
 
-    //A function to handle the post request
-    const putStudent = (toEditStudent) => {
-        return fetch(`http://localhost:8080/api/students/${toEditStudent.id}`, {
-            method: "PUT",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(toEditStudent),
-        })
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                onUpdateStudent(data);
-                //this line just for cleaning the form
-                clearForm();
-            });
-    };
+  const handlePositionChange = (event) => {
+    const position = event.target.value;
+    setAlumnus((alumnus) => ({ ...alumnus, position }));
+  };
 
+  const handleCompanyChange = (event) => {
+    const company = event.target.value;
+    setAlumnus((alumnus) => ({ ...alumnus, company }));
+  };
 
-    //A function to handle the submit in both cases - Post and Put request!
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (student.id) {
-            putStudent(student);
-        } else {
-            postStudent(student);
-        }
-    };
+  const handleSalaryChange = (event) => {
+    const salary = event.target.value;
+    setAlumnus((alumnus) => ({ ...alumnus, salary }));
+  };
 
-    return (
-        <Form className='form-students' onSubmit={handleSubmit}>
-            <Form.Group>
-                <Form.Label>First Name</Form.Label>
-                <input
-                    type="text"
-                    id="add-user-name"
-                    placeholder="First Name"
-                    required
-                    value={student.firstname}
-                    onChange={handleNameChange}
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Last Name</Form.Label>
-                <input
-                    type="text"
-                    id="add-user-lastname"
-                    placeholder="Last Name"
-                    required
-                    value={student.lastname}
-                    onChange={handleLastnameChange}
-                />
-            </Form.Group>
-            <Form.Check
-                type={'checkbox'}
-                id={`isCurrent`}
-                checked={student.is_current}
-                onChange={handleCheckChange}
-                label={`Are they in the current program?`}
-            />
-            <Form.Group>
-            <Button type="submit" variant="outline-success">{student.id ? "Edit Student" : "Add Student"}</Button>
-            {student.id ? <Button type="button" variant="outline-warning" onClick={clearForm}>Cancel</Button> : null}
-            </Form.Group>
-        </Form>
-    );
+  const handleDateChange = (event) => {
+    const date = event.target.value;
+    setAlumnus((alumnus) => ({ ...alumnus, date }));
+  };
+
+  const handleCheckChange = (event) => {
+    const is_looking = event.target.checked;
+    //console.log(iscurrent);
+    setAlumnus((alumnus) => ({ ...alumnus, is_looking }));
+  };
+
+  const handleLinkedinChange = (event) => {
+    const linkedin = event.target.checked;
+    //console.log(iscurrent);
+    setAlumnus((alumnus) => ({ ...alumnus, linkedin }));
+  };
+
+  const clearForm = () => {
+    setAlumnus({
+      name: "",
+      position: "",
+      company: "",
+      salary: "",
+      date: "",
+      is_looking: false,
+    });
+  };
+
+  //A function to handle the post request
+  const postAlumnus = (newAlumnus) => {
+    return fetch("http://localhost:8080/api/alumni", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newAlumnus),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        //console.log("From the post ", data);
+        //I'm sending data to the List of Alumnus (the parent) for updating the list
+        onSaveAlumnus(data);
+        //this line just for cleaning the form
+        clearForm();
+      });
+  };
+
+  //A function to handle the post request
+  const putAlumnus = (toEditAlumnus) => {
+    return fetch(`http://localhost:8080/api/alumni/${toEditAlumnus.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(toEditAlumnus),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        onUpdateAlumnus(data);
+        //this line just for cleaning the form
+        clearForm();
+      });
+  };
+
+  //A function to handle the submit in both cases - Post and Put request!
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (alumnus.id) {
+      putAlumnus(alumnus);
+    } else {
+      postAlumnus(alumnus);
+    }
+  };
+
+  return (
+    <Form className="form-students" onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label>Name</Form.Label>
+        <input
+          type="text"
+          id="add-user-name"
+          placeholder="Name"
+          required
+          value={alumnus.name}
+          onChange={handleNameChange}
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Current Position</Form.Label>
+        <input
+          type="text"
+          id="add-user-position"
+          placeholder="Current Position"
+          required
+          value={alumnus.position}
+          onChange={handlePositionChange}
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Company</Form.Label>
+        <input
+          type="text"
+          id="add-user-position"
+          placeholder="Company"
+          required
+          value={alumnus.company}
+          onChange={handleCompanyChange}
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Salary</Form.Label>
+        <input
+          type="text"
+          id="add-user-salary"
+          placeholder="Salary"
+          required
+          value={alumnus.salary}
+          onChange={handleSalaryChange}
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Start Date</Form.Label>
+        <input
+          type="date"
+          id="add-user-date"
+          placeholder="Date"
+          required
+          value={alumnus.date}
+          onChange={handleDateChange}
+        />
+      </Form.Group>
+
+      <Form.Check
+        type={"checkbox"}
+        id={`is_looking`}
+        checked={alumnus.is_looking}
+        onChange={handleCheckChange}
+        label={`Are you looking for work and opportunties?`}
+      />
+
+      <Form.Group>
+        <Form.Label>LinkedIn</Form.Label>
+        <input
+          type="text"
+          id="add-user-linkedin"
+          placeholder="LinkedIn"
+          required
+          value={alumnus.linkedin}
+          onChange={handleLinkedinChange}
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Button type="submit" variant="outline-success">
+          {alumnus.id ? "Edit Alumnus" : "Add Alumnus"}
+        </Button>
+        {alumnus.id ? (
+          <Button type="button" variant="outline-warning" onClick={clearForm}>
+            Cancel
+          </Button>
+        ) : null}
+      </Form.Group>
+    </Form>
+  );
 };
 
-
-export default MyForm
+export default MyForm;
