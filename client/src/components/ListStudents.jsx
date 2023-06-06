af18bc5f906e6d98e91c6431d868d37f33657a08
Gisselle Garcia <gissellegar46@gmail.com>
2023-06-05 17:05:46 -0700

    Add fetch requests to match backend, and create functions to save, delete, update on each click in card

--- client/src/components/ListStudents.jsx
+++ client/src/components/ListStudents.jsx
@@ -5,53 +5,53 @@ import Alumnus from "./Alumnus";
 
 const ListStudents = () => {
   // this is my original state with an array of alumni from get request
-  const [students, setStudents] = useState([]);
+  const [alumni, setAlumni] = useState([]);
 
   //this is the state needed for the UpdateRequest
-  const [editingStudent, setEditingStudent] = useState(null);
+  const [editingAlumnus, setEditingAlumnus] = useState(null);
 
-  const loadStudents = () => {
+  const loadAlumni = () => {
     // A function to fetch the list of students that will be load anytime that list change
     fetch("http://localhost:8080/api/alumni")
       .then((response) => response.json())
       .then((alumni) => {
-        setStudents(alumni);
+        setAlumni(alumni);
       });
   };
 
   useEffect(() => {
-    loadStudents();
-  }, [students]);
+    loadAlumni();
+  }, [alumni]);
 
-  const onSaveStudent = (newStudent) => {
+  const onSaveStudent = (newAlumnus) => {
     //console.log(newStudent, "From the parent - List of Students");
-    setStudents((students) => [...students, newStudent]);
+    setAlumni((alumni) => [...alumni, newAlumnus]);
   };
 
   //A function to control the update in the parent (student component)
-  const updateStudent = (savedStudent) => {
+  const updateAlumnus = (savedAlumnus) => {
     // console.log("Line 29 savedStudent", savedStudent);
     // This function should update the whole list of students -
-    loadStudents();
+    loadAlumni();
   };
 
   //A function to handle the Delete funtionality
-  const onDelete = (student) => {
+  const onDelete = (alumnus) => {
     //console.log(student, "delete method")
-    return fetch(`http://localhost:8080/api/students/${student.id}`, {
+    return fetch(`http://localhost:8080/api/alumni/${alumnus.id}`, {
       method: "DELETE",
     }).then((response) => {
       //console.log(response);
       if (response.ok) {
-        loadStudents();
+        loadAlumni();
       }
     });
   };
 
   //A function to handle the Update functionality
-  const onUpdate = (toUpdateStudent) => {
+  const onUpdate = (toUpdateAlumnus) => {
     //console.log(toUpdateStudent);
-    setEditingStudent(toUpdateStudent);
+    setEditingAlumnus(toUpdateAlumnus);
   };
 
   return (
@@ -66,19 +66,19 @@ const ListStudents = () => {
                 <li>three</li>
             </ul> */}
         {/* Commented this out in order to be able to view the rest of the page successfully<ul>
-                {students.map((student) => {
-                    return <li key={student.id}> <Student student={student} toDelete={onDelete} toUpdate={onUpdate} /></li>
+                {alumni.map((alumnus) => {
+                    return <li key={alumnus.id}> <Student alumnus={alumnus} toDelete={onDelete} toUpdate={onUpdate} /></li>
                 })}
             </ul> */}
       </div>
       <MyForm
-        key={editingStudent ? editingStudent.id : null}
-        onSaveStudent={onSaveStudent}
-        editingStudent={editingStudent}
-        onUpdateStudent={updateStudent}
+        key={editingAlumnus ? editingAlumnus.id : null}
+        onSaveAlumnus={onSaveAlumnus}
+        editingAlumnus={editingAlumnus}
+        onUpdateAlumnus={updateAlumnus}
       />
     </div>
   );
 };
 
-export default ListStudents;
+export default ListAlumnus;
